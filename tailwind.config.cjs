051307616fa7b297d825af2cac9afc680cbf04ca
Mohamed Abdi <mohamed.abdi11@hotmail.co.uk>
2023-06-07 16:02:09 +0100

    Navbar started

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
