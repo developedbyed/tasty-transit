b75a8a3373a9ae27f3fdebd6873160a931602145
Mohamed Abdi <mohamed.abdi11@hotmail.co.uk>
2023-06-08 15:51:37 +0100

    Home page Started adding Image to screen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,14 +10,19 @@ module.exports = {
         sans: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        'black-text': '#262626',
-        'grey-text': '#5E5E5E',
-        'footer-color': '#1D1D1D',
-        'primary-accent': '#FFAE4D'
+        "black-text": "#262626",
+        "grey-text": "#5E5E5E",
+        "footer-color": "#1D1D1D",
+        "primary-accent": "#FFAE4D",
+        "secondary-accent": "#7D2E89",
       },
       fontSize: {
-        xxs: '0.625rem',
-      }
+        xxs: "0.625rem",
+        hero: "3.5rem",
+      },
+      height: {
+        heroImage: "51.9375rem",
+      },
     },
   },
   plugins: [],
