74fbc743192b3744a41648c5e9d978bdbf2e101b
unknown <tombirbeck47@gmail.com>
2023-06-13 14:34:30 +0100

    finished styling the testimonials section

---
+++ src/components/testimonials.js
@@ -0,0 +1,29 @@
+const testimonials = [
+    {
+        name: 'MD. EARLY LEARNING EDUCATION',
+        message: 'The improvements have helped shape increased productivity, profits and performance. We would have no hesitation in recommending Karen to other businesses that are keen to develop and grow both in the market and their own talent.'
+    },
+    {
+        name: 'Name 2',
+        message: ' The improvements have helped shape increased productivity, profits and performance. We would have no hesitation in recommending Karen to other businesses that are keen to develop and grow both in the market and their own talent.'
+    },
+    {
+        name: 'Name 3',
+        message: ' The improvements have helped shape increased productivity, profits and performance. We would have no hesitation in recommending Karen to other businesses that are keen to develop and grow both in the market and their own talent.'
+    },
+    {
+        name: 'Name 4',
+        message: ' The improvements have helped shape increased productivity, profits and performance. We would have no hesitation in recommending Karen to other businesses that are keen to develop and grow both in the market and their own talent.'
+    },
+    {
+        name: 'Name 5',
+        message: ' The improvements have helped shape increased productivity, profits and performance. We would have no hesitation in recommending Karen to other businesses that are keen to develop and grow both in the market and their own talent.'
+    },
+    {
+        name: 'Name 6',
+        message: ' The improvements have helped shape increased productivity, profits and performance. We would have no hesitation in recommending Karen to other businesses that are keen to develop and grow both in the market and their own talent.'
+    },
+
+]
+
+export default testimonials
