2c557602d4736fb2d295874bda7bd851283d9443
unknown <tombirbeck47@gmail.com>
2023-06-22 00:09:11 +0100

    made hero logo slightly bigger, added x to hamburger menu

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -5,11 +5,17 @@ const Hamburger = () => {
     const [open, setOpen] = useState(false);
     return (
 <div className="min-w-max flex flex-col">
-    <div className="cursor-pointer md:hidden justify-end self-end" onClick={() => {setOpen(!open)}}>
+    {!open ? 
+    <div className="cursor-pointer md:hidden justify-end self-end" onClick={() => {setOpen(true)}}>
         <span className="block w-10 h-1 mb-2 bg-black"></span>
         <span className="block w-10 h-1 mb-2 bg-black"></span>
         <span className="block w-10 h-1 bg-black"></span>
+    </div> :
+    <div className="cursor-pointer md:hidden mt-3 justify-end self-end" onClick={() => {setOpen(false)}}>
+        <span className="block w-11 h-1 bg-black rotate-45 translate-y-1"></span>
+        <span className="block w-11 h-1 bg-black -rotate-45 -translate-x-0.5"></span>
     </div>
+    }
     {open && 
     <div className="flex flex-col gap-4 mt-4 z-10">
         <HamburgerItem href="/" title="Home" />
