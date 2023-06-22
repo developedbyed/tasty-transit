b5e825b2f07357282f99bf8337cb1e0e2d1f81f0
Mohamed Abdi <mohamed.abdi11@hotmail.co.uk>
2023-06-22 14:16:22 +0100

    Nav and Footer added to all pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
-
-  integrations: [tailwind(), react()]
-
+  integrations: [tailwind(), react()],
 });
